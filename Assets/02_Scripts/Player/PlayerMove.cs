4a10fd66e7e6f4e6a6ff0491b362e4135dfb02bb
ehdbs28 <doyun5373@naver.com>
2022-04-13 20:30:00 +0900

    플레이어 점프, 에니메이션 기초작업

--- Assets/02_Scripts/Player/PlayerMove.cs
+++ Assets/02_Scripts/Player/PlayerMove.cs
@@ -12,11 +12,15 @@ public class PlayerMove : MonoBehaviour
     private bool isGround = false;
     private Vector3 footPosition;
     private CapsuleCollider2D capsuleCollider2D;
+    private Animator anim = null;
+    private SpriteRenderer sprite = null;
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     
@@ -26,10 +30,17 @@ public class PlayerMove : MonoBehaviour
         footPosition = new Vector2(bounds.center.x, bounds.min.y);
         isGround = Physics2D.OverlapCircle(footPosition, 0.1f, groundLayer);
         float h = Input.GetAxisRaw("Horizontal");
+        if(h != 0)
+        {
+            anim.Play("PlayerMove");
+        }else
+        {
+            anim.Play("PlayerIdle");
+        }
         transform.Translate(new Vector2(h, 0) * speed * Time.deltaTime);
 
-        
-        Debug.Log(isGround);
+        //반대방향 돌리기 예정
+
         if (isGround &&Input.GetButton("Jump"))
         {
             rigid.velocity = Vector2.up * jumpPower;
