edb7cfe8a7edcb3a8a7c2c037db4a54a2631e619
ehdbs28 <doyun5373@naver.com>
2022-04-14 20:11:48 +0900

    Player DeathAnimation

--- Assets/02_Scripts/Player/PlayerMove.cs
+++ Assets/02_Scripts/Player/PlayerMove.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMove : MonoBehaviour
 {
+    
     public float speed;
     public float jumpPower;
-    Rigidbody2D rigid;
     public float groundRayDistance = 1f;
     public LayerMask groundLayer;
     private bool isGround = false;
     private Vector3 footPosition;
     private CapsuleCollider2D capsuleCollider2D;
     private Animator anim = null;
-    private bool isJump = false;
+    private Rigidbody2D rigid;
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -21,18 +21,23 @@ public class PlayerMove : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    
+
     void Update()
     {
         Move();
-        if (isGround &&Input.GetButton("Jump"))
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (isGround && Input.GetButton("Jump"))
         {
             anim.SetBool("isJump", true);
-            isJump = true;
             rigid.velocity = Vector2.zero;
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
     }
+
     private void Move()
     {
         Bounds bounds = capsuleCollider2D.bounds;
@@ -43,17 +48,28 @@ public class PlayerMove : MonoBehaviour
         {
             anim.SetBool("isJump", false);
             if (h != 0)
-            {
-                anim.SetBool("isMove", true);
-            }
+                anim.SetBool("isMove", true); 
             else
                 anim.SetBool("isMove", false);
         }
+
         if (h < 0)
             transform.localScale = new Vector3(-1, 1, 1);
         else
             transform.localScale = new Vector3(1, 1, 1);
         transform.Translate(new Vector2(h, 0) * speed * Time.deltaTime);
     }
-    
+
+    private void DeathAnimator()
+    {
+        anim.Play("PlayerDie");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Trap"))
+        {
+            DeathAnimator();
+        }
+    }
 }
