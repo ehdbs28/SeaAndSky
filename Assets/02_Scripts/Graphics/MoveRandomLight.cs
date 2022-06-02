8392f50bf76f042e14013aa775e0c857baf4f408
minyoung529 <dlalsdud102613@gmail.com>
2022-06-02 21:16:18 +0900

    빛 구현 완료

---
+++ Assets/02_Scripts/Graphics/MoveRandomLight.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class MoveRandomLight : MonoBehaviour
+{
+    private float timer = 0f;
+    private float randomTime;
+
+    private float randomIntensity = 0f;
+
+    new private Light2D light;
+    private Vector3 targetPosition;
+
+    private void Start()
+    {
+        light = GetComponent<Light2D>();
+        SetRandomValue();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > randomTime)
+        {
+            SetRandomValue();
+        }
+        else
+        {
+            light.intensity = Mathf.Lerp(light.intensity, randomIntensity, timer / randomTime);
+            transform.position = Vector2.Lerp(transform.position, targetPosition, timer / randomTime);
+        }
+    }
+
+    private void SetRandomValue()
+    {
+        randomTime = Random.Range(3f, 5f);
+        randomIntensity = Random.Range(0.6f, 1f);
+        targetPosition = transform.position;
+        targetPosition.x += Random.Range(-0.05f, 0.05f);
+        timer = 0f;
+    }
+}
