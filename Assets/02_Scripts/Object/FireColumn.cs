a493d53dfed1b18a47a7e5a3efbe17e0a753c7cc
minyoung529 <dlalsdud102613@gmail.com>
2022-06-16 20:15:17 +0900

    불기둥 완료

---
+++ Assets/02_Scripts/Object/FireColumn.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FireColumn : MonoBehaviour
+{
+    private WaitForSeconds delay = new WaitForSeconds(3f);
+    private readonly int fireHash = Animator.StringToHash("Fire");
+
+    private Animator animator;
+
+    new private SpriteRenderer renderer;
+    new private Collider2D collider;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        renderer = GetComponentInChildren<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
+
+        renderer.sprite = null;
+        StartCoroutine(ActiveColumn());
+    }
+
+    private IEnumerator ActiveColumn()
+    {
+        while (true)
+        {
+            renderer.enabled = true;
+            collider.enabled = true;
+            animator.SetTrigger(fireHash);
+            yield return delay;
+
+            renderer.enabled = false;
+            collider.enabled = false;
+            yield return delay;
+        }
+    }
+}
