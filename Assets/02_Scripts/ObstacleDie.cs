3d80e9883cccc7eefe7db5f2e9d964959bc9d3e7
minchae <98925762+minchae123@users.noreply.github.com>
2022-06-30 20:26:40 +0900

    Blade 움직임 수정

--- Assets/02_Scripts/ObstacleDie.cs
+++ Assets/02_Scripts/ObstacleDie.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum TrapType
+{
+    Blade,
+    Spike
+}
 public class ObstacleDie : MonoBehaviour
 {
+    public TrapType _trapType;
+    public float offsetX = 1f;  
+    float currentPosition;  
+    float speed = 2.0f;  
 
+    Vector3 originPos;
 
     [Header("OnOff¿©ºÎ")]
     public bool isToggle;
@@ -18,25 +27,52 @@ public class ObstacleDie : MonoBehaviour
 
     private void Awake()
     {
+        originPos = transform.position;
         animator = GetComponent<Animator>();
+        currentPosition = transform.position.x;
+        isActive = isToggle == true ? false : true;
     }
 
     public void Update()
     {
         OnOffTimer();
+        MoveBlade();
     }
+
+    public void MoveBlade(){
+        if(_trapType == TrapType.Blade)
+        {
+            currentPosition += Time.deltaTime * speed;
+            if (currentPosition >= originPos.x)
+            {
+                speed *= -1;
+                currentPosition = originPos.x;
+            }
+            else if (currentPosition <= originPos.x -offsetX)
+            {
+                speed *= -1;
+                currentPosition = originPos.x - offsetX;
+            }
+            transform.position = new Vector3(currentPosition, transform.position.y, 0);
+        }
+    }
+
     public void OnTrapAnim()
     {
         ChangeActive(true);
     }
+
     public void OffTrapAnim()
     {
         ChangeActive(false);
     }
+
     private void OnOffTimer()
     {
+        if (_trapType != TrapType.Spike) return;
         if (isToggle == true)
         {
+            //Debug.Log(gameObject.name);
             timer += Time.deltaTime;
             if (timer >= timeDelay)
             {
@@ -60,10 +96,10 @@ public class ObstacleDie : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Player") && isActive)
-        {
-            GameManager.Instance.ReduceHeart();
-        }
+            if(collision.collider.CompareTag("Player") && isActive)
+            {
+                GameManager.Instance.ReduceHeart();
+            }
     }
 
 }
