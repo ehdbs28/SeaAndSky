213216155d3effa48881e3574e299abbb653b4e0
minyoung529 <dlalsdud102613@gmail.com>
2022-08-04 21:03:48 +0900

    스버다고

--- Assets/02_Scripts/UI/StageButton.cs
+++ Assets/02_Scripts/UI/StageButton.cs
@@ -8,18 +8,23 @@ public class StageButton : MonoBehaviour
 {
     private Text stageText;
     private Button button;
+    int index;
 
     public void Init(int index)
     {
         stageText ??= GetComponentInChildren<Text>();
         button ??= GetComponent<Button>();
+        this.index = index;
 
-        DataManager.Instance.User.stage = index;
-        button.onClick.AddListener(() => SceneManager.LoadScene("Main"));
+        button.onClick.AddListener(() =>
+        {
+            DataManager.Instance.User.stage = this.index;
+            SceneManager.LoadScene("Main");
+        });
 
         stageText.text = index.ToString();
 
-        if(DataManager.Instance.User.maxStage < index)
+        if (DataManager.Instance.User.maxStage < index)
         {
             button.interactable = false;
         }
