010a647277610bf432f3d62bbff93f853bfbe780
minyoung529 <dlalsdud102613@gmail.com>
2022-08-04 19:34:08 +0900

    아까 푸시 못 한 거

--- Assets/02_Scripts/Object/FireColumn.cs
+++ Assets/02_Scripts/Object/FireColumn.cs
@@ -44,7 +44,7 @@ public class FireColumn : MonoBehaviour
             animator.SetTrigger(fireHash);
 
             // 나중에 사용자 볼륨 추가
-            float volume = 1f - Mathf.Lerp(0f, 1f, Vector2.Distance(transform.position, player.position) / 10f) * 1.15f;
+            float volume = (1f - Mathf.Lerp(0f, 1f, Vector2.Distance(transform.position, player.position) / 10f)) * 1.15f;
 
             audioSource.PlayOneShot(fireSound, volume);
 
