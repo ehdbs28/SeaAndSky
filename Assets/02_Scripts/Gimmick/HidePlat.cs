9296b65c26726fd7a85436737625b19618cd198c
minchae123 <08minchae17@gmail.com>
2022-09-21 18:33:22 +0900

    사라지는 구름 제작

--- Assets/02_Scripts/Gimmick/HidePlat.cs
+++ Assets/02_Scripts/Gimmick/HidePlat.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class HidePlat : MonoBehaviour
 {
-    [SerializeField] private float fallTime = 0.7f;
     [SerializeField] private float destroyTime = 3f;
+    [SerializeField] private float spawnTime = 5f;
+    [SerializeField] private float durationTime = 2f;
+    [SerializeField] private GameObject plat;
+    private SpriteRenderer sp;
+    private BoxCollider2D col;
+    private Vector3 dir;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
     {
-        if (collision.CompareTag("Player"))
-        {
+        dir = plat.transform.position;
+        sp = GetComponent<SpriteRenderer>();
+        col = GetComponent<BoxCollider2D>();
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            StartCoroutine(HideTime());
         }
+    }
+
+    IEnumerator HideTime()
+    { 
+        yield return new WaitForSeconds(destroyTime);
+
+        plat.transform.DOMoveY(transform.position.y - 3, durationTime);
+        sp.DOFade(0, durationTime);
+        col.enabled = false;
+        yield return new WaitForSeconds(spawnTime);
+
+        col.enabled = true;
+        plat.transform.position = dir;
+        sp.color = new Color(1, 1, 1, 1);
     }
 }
