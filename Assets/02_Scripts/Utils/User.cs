fda8a3c0116ed891f824a3c6f3dc4f32697e53a7
ehdbs28 <doyun5373@naver.com>
2022-12-26 12:36:45 +0900

    Thanks for playing our game 텍스트 추가
    에니매이션은 넣어야함

--- Assets/02_Scripts/Utils/User.cs
+++ Assets/02_Scripts/Utils/User.cs
@@ -6,10 +6,11 @@ public class User
 {
     //Max Stage
     private const int StageLimit = 6;       //Don't change it unless you add a stage
+    public int stageLimit => StageLimit;
+    public bool isClearGame => maxStage == StageLimit;
 
     public int stage = 1;
     public int maxStage = 1;
-    public int stageLimit => StageLimit;
     public KeyCode[] keySetting = new KeyCode[(int)Key.keycount];
     public List<int> selectedBGM = new List<int>();
 }
