7c0af8a1572e2a0249efd7e119bf4438559ea8c8
ehdbs28 <doyun5373@naver.com>
2022-12-26 11:31:29 +0900

    Stage Limit 추가

--- Assets/02_Scripts/Utils/User.cs
+++ Assets/02_Scripts/Utils/User.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class User
 {
+    //Max Stage
+    private const int StageLimit = 6;
+
     public int stage = 1;
     public int maxStage = 1;
+    public int stageLimit => StageLimit;
     public KeyCode[] keySetting = new KeyCode[(int)Key.keycount];
     public List<int> selectedBGM = new List<int>();
 }
